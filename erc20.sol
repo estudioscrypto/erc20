128b97da683ed97ea5332927974350c02606451c
manuel perez <manuelperez.0000gmail.com>
2022-11-21 12:29:48 -0400

    Requies added

--- erc20.sol
+++ erc20.sol
@@ -4,7 +4,7 @@
 //contact: manudev.oficial@gmail.com
 //standard erc20 token
 
-pragma solidity ^0.8.10;
+pragma solidity ^0.8.17;
 
 interface IERC20 {
     function totalSupply() external view returns (uint);
@@ -13,38 +13,52 @@ interface IERC20 {
     function allowance(address owner, address spender) external view returns (uint);
     function approve(address spender, uint amount) external;
     function transferFrom( address sender,address recipient,uint amount ) external;
+    event Mint(address indexed from, address indexed to, uint value);
+    event Burn(address indexed from, address indexed to, uint value);
     event Transfer(address indexed from, address indexed to, uint value);
     event Approval(address indexed owner, address indexed spender, uint value);
 }
 
-contract CCT is IERC20 {
+contract EST is IERC20 {
 
-    string public name = "Token name";
-    string public symbol = "SYMBOL";
+    string public name = "Estudio Token";
+    string public symbol = "EST";
     uint8 public decimals = 18;
-    uint public totalSupply = 1000000*10**18;
+    uint public totalSupply = 1000*10**18;
     mapping(address => uint) public balanceOf;
     mapping(address => mapping(address => uint)) public allowance;
-    //address public Owner;
+    address public Owner;
 
     constructor() {
-        //Owner = msg.sender;
+        Owner = msg.sender;
         balanceOf[msg.sender] = totalSupply;
         emit Transfer(address(0), msg.sender, totalSupply);
     }
 
+    modifier onlyOwner() {
+        require(msg.sender == Owner, "ERROR: YOU NOT OWNER");
+        _;
+    }
+
     function transfer(address recipient, uint amount) public {
+        require(recipient != address(0),"ERROR: zero direction not allowed");
+        require(balanceOf[msg.sender] >= amount,"ERROR: insufficient balance");
         balanceOf[msg.sender] -= amount;
         balanceOf[recipient] += amount;
         emit Transfer(msg.sender, recipient, amount);
     }
 
     function approve(address spender, uint amount) external {
+        require(spender != address(0),"ERROR: zero direction not allowed");
+        require(balanceOf[msg.sender] >= amount,"ERROR: insufficient balance");
         allowance[msg.sender][spender] = amount;
         emit Approval(msg.sender, spender, amount);
     }
 
     function transferFrom( address sender, address recipient, uint amount ) external {
+        require(sender != address(0),"ERROR: zero direction not allowed");
+        require(recipient != address(0),"ERROR: zero direction not allowed");
+        require(allowance[msg.sender][sender] >= amount,"ERROR: You do not have permissions to send these tokens");
         allowance[sender][msg.sender] -= amount;
         balanceOf[sender] -= amount;
         balanceOf[recipient] += amount;
@@ -54,19 +68,12 @@ contract CCT is IERC20 {
     function burn(uint amount) public {
         balanceOf[msg.sender] -= amount;
         totalSupply -= amount;
-        emit Transfer(msg.sender, address(0), amount);
+        emit Burn(msg.sender, address(0), amount);
     }
 
-    /*
-    modifier onlyOwner() {
-        require(msg.sender == Owner, "ERROR: YOU NOT OWNER");
-        _;
-    }
-    
     function mint(uint amount) public onlyOwner{
         totalSupply = totalSupply+amount;
-        balanceOf[msg.sender] += amount;
-        emit Transfer(address(0), Owner, amount);
+        balanceOf[Owner] += amount;
+        emit Mint(address(0), Owner, amount);
     }
-    */
 }
